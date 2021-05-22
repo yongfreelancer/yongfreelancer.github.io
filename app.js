1ac49d65ce52f6de55f67daf35c8b4f04c69130a
yongfreelancer <yongfreelancer@gmail.com>
2021-05-22 10:11:49 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
     data(){
         return {
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'John@gmail.com',
+            firstName: 'Moeses Reo',
+            lastName: 'Barluado',
+            email: 'yongfreelancer@gmail.com',
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg'
         }
