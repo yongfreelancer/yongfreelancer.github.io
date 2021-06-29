73368fdbcc86614997c93ab4b539d6cce3d918d1
yongfreelancer <yongfreelancer@gmail.com>
2021-06-30 01:03:43 +0800

    Added Shopify and WooCommerce Icons

--- js/script.js
+++ js/script.js
@@ -11,25 +11,46 @@
          path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
          path.style["animation-delay"] = `${i * delay}s`;
      }
+
  }
  setTextAnimation(0.1, 0.5, 2, 'linear', '#ffffff', false);
 
  $(document).ready(function () {
 
-    new fullpage('#fullpage', {
-        anchors: ['page1', 'page2', 'page3', 'page4'],
-        navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
-        css3: true,
-        scrollingSpeed: 1000,
-        navigation: false,
-        slidesNavigation: true,
-        responsiveHeight: 330,
-        dragAndMove: true,
-        dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
-        controlArrows: false,
-        fadingEffect: true,
-		fadingEffectKey: 'YWx2YXJvdHJpZ28uY29tXzAzN1ptRmthVzVuUldabVpXTjBiNXo='
-    });
-    
+     new fullpage('#fullpage', {
+         anchors: ['page1', 'page2', 'page3', 'page4'],
+         navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+         css3: true,
+         scrollingSpeed: 1000,
+         navigation: false,
+         slidesNavigation: true,
+         responsiveHeight: 330,
+         dragAndMove: true,
+         dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+         controlArrows: false,
+         slidesNavigation: true,
+     });
+     $('html body').on('scroll touchmove mousewheel', function (e) {
+         if (!$(".dev-icons").hasClass('visible') && window.location.hash == "") {
+             e.preventDefault();
+             e.stopPropagation();
+             return false;
+         }
+
+     })
+     $(".dev-icons").fadeIn(4000, function () {
+         $(this).addClass('visible');
+     })
+
+
+
 
  });
+
+ function disableScroll() {
+     $('html body').on('scroll touchmove mousewheel', function (e) {
+         e.preventDefault();
+         e.stopPropagation();
+         return false;
+     })
+ }
